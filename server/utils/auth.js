90784da3562070535184e348f2460dc3e4f33159
Menji <jennifer.menjivar18@gmail.com>
2021-09-14 16:40:55 -0400

    added react and used concurrently

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,5 @@
 const jwt = require("jsonwebtoken");
-// require("dotenv").config(); j
-
-const secret = "mysecretsshhhh";
-// const secret = process.env.THE_SECRET;
+require("dotenv").config();
 
 const expiration = "2h";
 
@@ -10,7 +7,9 @@ module.exports = {
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({ data: payload }, `${process.env.THE_SECRET}`, {
+      expiresIn: expiration,
+    });
   },
 
   authMiddleware: function ({ req }) {
